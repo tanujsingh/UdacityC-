c1237045fc3582f555be643d5ac9ab01aa625f27
tanujsingh <tanujsingh.iiita@gmail.com>
2020-09-01 12:26:37 -0700

    Done

--- functions/main.hpp
+++ functions/main.hpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
+
 void printProduct(int a, int b, int c);
+void printEquation(int m1, int m2, int result, char operation);
+int sum(int m1, int m2);
 
 void printProduct(int a,int b,int c) {
     cout<<a<<" * "<<b<<" = "<<c<<"\n";
+}
+
+void printEquation(int m1, int m2, int result, char operation) {
+    cout<<m1<<" "<<operation<<" "<<m2<<" = "<<result<<"\n";
+}
+
+int sum(int m1, int m2) {
+    return m1 + m2;
 }
---
+++ functions/printEquation.cpp
@@ -0,0 +1,10 @@
+#include "main.hpp"
+
+int main() {
+    int m1 = 4;
+    int m2 = 6;
+    
+    printEquation(m1, m2, sum(m1, m2),'+');
+
+    return 0;
+}
